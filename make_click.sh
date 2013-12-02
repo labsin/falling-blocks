2907495c9eff10f4bdca67c710dbd816ddfe491f
Sam Segers <sam.sgrs@gmail.com>
2013-12-02 20:12:36 +0100

    Add deploy scripts

---
+++ make_click.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+FROMDIR="$(pwd)"
+BUILDDIR="${FROMDIR}_click"
+NAME="solitaire-games"
+rm -rf $BUILDDIR
+cp -r $FROMDIR $BUILDDIR
+echo "Entering ${BUILDDIR}"
+cd $BUILDDIR
+rm -rf .git* .bzr* debian desktop ${NAME}.qmlp* \
+${NAME}128.png ${NAME}16.png ${NAME}32.png \
+${NAME}64.png make_click.sh README.md LICENSE
+cd po
+echo "Generating mo files"
+./generate_mo.sh
+cd ..
+rm -r po
+cd ..
+echo "Build click package in `pwd`"
+click build $BUILDDIR
+cd $FROMDIR
+
