2ced267c9dd5b2273c765a19349e0dc17f8b03f6
labsin <sam.sgrs@gmail.com>
2015-04-18 20:08:17 +0200

    Add pot generation script

---
+++ po/generate_pot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pushd `dirname "${BASH_SOURCE[0]}"`/..
+xgettext -C --qt --keyword=tr -d fallingBlocks -o po/fallingBlocks.pot  */*.qml *.qml
+echo "updating translation files"
+for i in po/*.po; do echo "updating $i"; msgmerge -U $i po/fallingBlocks.pot; done
+popd
